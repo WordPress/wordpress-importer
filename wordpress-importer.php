0f8c2a1042d9269a66bd7574985537d7b88200d6
Dion Hulse <contact@dd32.id.au>
2017-12-16 00:20:30 +1100

    Add a loader plugin file for those who install the git repository into wp-content/plugins/

---
+++ wordpress-importer.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ * Plugin Name: WordPress Importer Git loader
+ */
+
+// This file is included purely for those using Git and checking out directly into wp-content/plugins/wordpress-importer/
+include dirname( __FILE__ ) . '/src/wordpress-importer.php';
