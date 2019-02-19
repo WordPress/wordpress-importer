4a9eee578c91b33dbb82ae5226411dc38c06ebad
Jonathan Sadowski <sadowski@automattic.com>
2019-02-19 11:45:50 -0600

    Separate store into store.js

---
+++ src/store.js
@@ -0,0 +1,42 @@
+/**
+ * External dependencies
+ */
+import { registerStore } from '@wordpress/data';
+
+const DEFAULT_STATE = {
+	nonce: null,
+	importAuthors: [],
+};
+
+registerStore( 'wordpress-importer', {
+	reducer( state = DEFAULT_STATE, action ) {
+		switch ( action.type ) {
+			case 'PROCESS_UPLOAD':
+				return {
+					...state,
+					nonce: action.nonce,
+					importAuthors: action.importAuthors,
+				};
+		}
+		return state;
+	},
+
+	actions: {
+		setUploadResult( result ) {
+			return {
+				type: 'PROCESS_UPLOAD',
+				nonce: result.nonce,
+				importAuthors: result.authors,
+			};
+		},
+	},
+
+	selectors: {
+		getNonce( state ) {
+			return state.nonce;
+		},
+		getImportAuthors( state ) {
+			return state.importAuthors;
+		},
+	}
+} );
