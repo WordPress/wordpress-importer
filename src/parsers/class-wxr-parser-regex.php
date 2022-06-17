2d9719ad8d83ecf3106e999627e297e004488131
jrfnl <jrfnl@users.noreply.github.com>
2022-06-17 02:55:41 +0200

    WXR_Parser_Regex: explicitly declare all properties
    Dynamic (non-explicitly declared) property usage is expected to be deprecated as of PHP 8.2 and are expected to become a fatal error in PHP 9.0.
    
    There are a number of ways to mitigate this:
    * If it's an accidental typo for a declared property: fix the typo.
    * For known properties: declare them on the class.
    * For unknown properties: add the magic `__get()`, `__set()` et al methods to the class or let the class extend `stdClass` which has highly optimized versions of these magic methods build in.
    * For unknown _use of_ dynamic properties, the `#[AllowDynamicProperties]` attribute can be added to the class. The attribute will automatically be inherited by child classes.
    
    In this case, the `__construct()` method explicitly sets this property, so `$has_gzip` falls in the "known property" category and should be explicitly declared.
    
    Refs:
    * https://wiki.php.net/rfc/deprecate_dynamic_properties

--- src/parsers/class-wxr-parser-regex.php
+++ src/parsers/class-wxr-parser-regex.php
@@ -10,13 +10,14 @@
  * WXR Parser that uses regular expressions. Fallback for installs without an XML parser.
  */
 class WXR_Parser_Regex {
-	var $authors       = array();
-	var $posts         = array();
-	var $categories    = array();
-	var $tags          = array();
-	var $terms         = array();
-	var $base_url      = '';
-	var $base_blog_url = '';
+	public $authors       = array();
+	public $posts         = array();
+	public $categories    = array();
+	public $tags          = array();
+	public $terms         = array();
+	public $base_url      = '';
+	public $base_blog_url = '';
+	public $has_gzip;
 
 	function __construct() {
 		$this->has_gzip = is_callable( 'gzopen' );
