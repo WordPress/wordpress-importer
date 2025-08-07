911d78b5b0760752e82f5195e0329a8c4edc01f4
Adam Zieliński <adam@adamziel.com>
2025-08-07 18:08:40 +0200

    Initial stab at bringing XMLProcessor over

---
+++ src/parsers/xml-processor/BaseByteReadStream.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace WordPress\ByteStream\ReadStream;
+
+use WordPress\ByteStream\ByteStreamException;
+use WordPress\ByteStream\NotEnoughDataException;
+
+abstract class BaseByteReadStream implements ByteReadStream {
+
+	const CHUNK_SIZE = 100;// 64 * 1024;
+
+	protected $buffer_size = 2048;
+
+	protected $buffer = '';
+	protected $offset_in_current_buffer = 0;
+	protected $bytes_already_forgotten = 0;
+	protected $is_closed = false;
+	protected $expected_length = null;
+
+	public function length(): ?int {
+		return $this->expected_length;
+	}
+
+	public function pull( $n = self::CHUNK_SIZE, $mode = self::PULL_NO_MORE_THAN ): int {
+		switch ( $mode ) {
+			case self::PULL_NO_MORE_THAN:
+			case self::PULL_EXACTLY:
+				break;
+			default:
+				throw new ByteStreamException( 'Invalid pull mode' );
+		}
+		if ( $this->is_closed ) {
+			throw new ByteStreamException( 'Cannot pull() on a closed producer' );
+		}
+
+		if ( $n === 0 ) {
+			return 0;
+		}
+
+		if ( $n < 0 ) {
+			throw new ByteStreamException( 'Cannot pull a negative number of bytes' );
+		}
+
+		if ( $n <= $this->count_consumable_bytes() ) {
+			return $n;
+		}
+
+		if ( $this->reached_end_of_data() ) {
+			if ( $mode === ByteReadStream::PULL_EXACTLY ) {
+				throw new NotEnoughDataException( 'End of data reached while pulling' );
+			}
+
+			return 0;
+		}
+
+		if ( $mode === ByteReadStream::PULL_NO_MORE_THAN ) {
+			return $this->pull_no_more_than( $n );
+		}
+
+		return $this->pull_exactly( $n );
+	}
+
+	protected function pull_exactly( $n ): int {
+		$empty_pulls = 0;
+		while ( $this->count_consumable_bytes() < $n ) {
+			$consumable_before = $this->count_consumable_bytes();
+			$this->pull_no_more_than( $n );
+			$consumable_after = $this->count_consumable_bytes();
+
+			if ( $consumable_after === $consumable_before ) {
+				++ $empty_pulls;
+				if ( $this->reached_end_of_data() ) {
+					throw new NotEnoughDataException( 'End of data reached while pulling' );
+				}
+			}
+
+			if ( $empty_pulls > 4 ) {
+				throw new NotEnoughDataException( '4 empty pulls in a row, we are probably at the end of the data' );
+			}
+		}
+
+		return $n;
+	}
+
+	protected function pull_no_more_than( $n ): int {
+		$this->buffer .= $this->internal_pull( self::CHUNK_SIZE );
+
+		return min( $n, $this->count_consumable_bytes() );
+	}
+
+	public function consume_all(): string {
+		$body = '';
+		while ( true ) {
+			if ( $this->reached_end_of_data() ) {
+				return $body;
+			}
+			$consumable = $this->pull( 64 * 1024 );
+			$body       .= $this->consume( $consumable );
+		}
+	}
+
+	protected function count_consumable_bytes(): int {
+		return strlen( $this->buffer ) - $this->offset_in_current_buffer;
+	}
+
+	abstract protected function internal_pull( $n ): string;
+
+	public function peek( $n ): string {
+		return substr( $this->buffer, $this->offset_in_current_buffer, $n );
+	}
+
+	public function consume( $n ): string {
+		if ( strlen( $this->buffer ) < $this->offset_in_current_buffer + $n ) {
+			throw new NotEnoughDataException( 'Cannot consume more bytes than available in the buffer.' );
+		}
+		$bytes                          = substr( $this->buffer, $this->offset_in_current_buffer, $n );
+		$this->offset_in_current_buffer += $n;
+		if ( $this->offset_in_current_buffer > $this->buffer_size ) {
+			$overflow                       = $this->offset_in_current_buffer - $this->buffer_size;
+			$this->offset_in_current_buffer -= $overflow;
+			$this->bytes_already_forgotten  += $overflow;
+			$this->buffer                   = substr( $this->buffer, $overflow );
+		}
+
+		return $bytes;
+	}
+
+	public function seek( int $target_offset ): void {
+		// We have that offset in the buffer, let's just update the pointer
+		if ( $target_offset >= $this->bytes_already_forgotten && $target_offset <= $this->bytes_already_forgotten + strlen( $this->buffer ) ) {
+			$this->offset_in_current_buffer = $target_offset - $this->bytes_already_forgotten;
+
+			return;
+		}
+		if ( null !== $this->length() && $target_offset > $this->length() ) {
+			$length = $this->length();
+			throw new NotEnoughDataException( sprintf( 'Cannot seek to past the stream length (seeked to %d, stream length is %d).',
+				$target_offset, $length ) );
+		}
+
+		if ( $target_offset < 0 ) {
+			throw new ByteStreamException( 'Cannot seek to a negative offset' );
+		}
+
+		// Seeking outside of buffer range, we need a producer-specific implementation
+		$this->seek_outside_of_buffer( $target_offset );
+	}
+
+	protected function seek_outside_of_buffer( int $target_offset ): void {
+		throw new ByteStreamException( 'Cannot seek outside of the buffered range' );
+	}
+
+	public function tell(): int {
+		return $this->bytes_already_forgotten + $this->offset_in_current_buffer;
+	}
+
+	public function reached_end_of_data(): bool {
+		if ( $this->is_closed ) {
+			return true;
+		}
+		if ( $this->count_consumable_bytes() > 0 ) {
+			return false;
+		}
+		if ( null !== $this->length() ) {
+			return $this->tell() >= $this->length();
+		}
+
+		return $this->internal_reached_end_of_data();
+	}
+
+	protected function internal_reached_end_of_data(): bool {
+		return false;
+	}
+
+	public function close_reading(): void {
+		$this->is_closed = true;
+	}
+
+	protected function internal_close_reading(): void {
+	}
+}
---
+++ src/parsers/xml-processor/load.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/ByteReadStream.php';
+require_once __DIR__ . '/BaseByteReadStream.php';
+require_once __DIR__ . '/FileReadStream.php';
+require_once __DIR__ . '/ByteStreamException.php';
+require_once __DIR__ . '/NotEnoughDataException.php';
+require_once __DIR__ . '/XMLDecoder.php';
+require_once __DIR__ . '/XMLAttributeToken.php';
+require_once __DIR__ . '/XMLElement.php';
+require_once __DIR__ . '/XMLUnsupportedException.php';
+require_once __DIR__ . '/XMLProcessor.php';
+require_once __DIR__ . '/EntityReader.php';
+require_once __DIR__ . '/WXREntityReader.php';
+require_once __DIR__ . '/utf8_decoder.php';
+require_once __DIR__ . '/ImportEntity.php';
