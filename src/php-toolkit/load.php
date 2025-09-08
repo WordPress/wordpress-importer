8511eb04e9f20a0cf33d8f122de2ae532edc1e6e
Adam Zieliński <adam@adamziel.com>
2025-09-09 00:37:49 +0200

    do not load the normalizer class when one is already loaded

--- src/php-toolkit/load.php
+++ src/php-toolkit/load.php
@@ -2,8 +2,11 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-require_once __DIR__ . '/DataLiberation/vendor-patched/symfony/polyfill-intl-normalizer/Normalizer.php';
-require_once __DIR__ . '/DataLiberation/vendor-patched/symfony/polyfill-intl-normalizer/Resources/stubs/Normalizer.php';
+if ( ! class_exists( '\Normalizer', false ) ) {
+	require_once __DIR__ . '/DataLiberation/vendor-patched/symfony/polyfill-intl-normalizer/Normalizer.php';
+	require_once __DIR__ . '/DataLiberation/vendor-patched/symfony/polyfill-intl-normalizer/Resources/stubs/Normalizer.php';
+}
+
 require_once __DIR__ . '/DataLiberation/vendor-patched/symfony/polyfill-intl-normalizer/bootstrap.php';
 require_once __DIR__ . '/DataLiberation/vendor-patched/symfony/polyfill-ctype/bootstrap.php';
 require_once __DIR__ . '/DataLiberation/vendor-patched/symfony/polyfill-php80/bootstrap.php';
