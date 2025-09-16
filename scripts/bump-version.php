8399e220ced59b222ed898d06f5f4f8f6e9af23c
Adam Zieliński <adam@adamziel.com>
2025-09-16 15:02:20 +0200

    PHPCS

--- scripts/bump-version.php
+++ scripts/bump-version.php
@@ -4,54 +4,54 @@ declare(strict_types=1);
 
 $usage = "Usage: composer run bump-version -- <version|major|minor|patch>\n";
 
-if ($argc < 2) {
-    fwrite(STDERR, $usage);
-    exit(1);
+if ( $argc < 2 ) {
+	fwrite( STDERR, $usage );
+	exit( 1 );
 }
 
-$root = dirname(__DIR__);
-$files = [
-    'wordpress-importer.php' => $root . DIRECTORY_SEPARATOR . 'wordpress-importer.php',
-    'src/wordpress-importer.php' => $root . DIRECTORY_SEPARATOR . 'src/wordpress-importer.php',
-    'src/readme.txt' => $root . DIRECTORY_SEPARATOR . 'src/readme.txt',
-];
+$root  = dirname( __DIR__ );
+$files = array(
+	'wordpress-importer.php'     => $root . DIRECTORY_SEPARATOR . 'wordpress-importer.php',
+	'src/wordpress-importer.php' => $root . DIRECTORY_SEPARATOR . 'src/wordpress-importer.php',
+	'src/readme.txt'             => $root . DIRECTORY_SEPARATOR . 'src/readme.txt',
+);
 
-preg_match('/\* Version: (\d+\.\d+\.\d+)/', file_get_contents($files['wordpress-importer.php']), $matches);
+preg_match( '/\* Version: (\d+\.\d+\.\d+)/', file_get_contents( $files['wordpress-importer.php'] ), $matches );
 $current_version = $matches[1];
 
-list($major, $minor, $patch) = explode('.', $current_version);
-switch ($argv[1] ?? 'patch') {
-    case 'patch':
-        $patch++;
-        break;
-    case 'major':
-        $major++;
-        break;
-    case 'minor':
-        $minor++;
-        break;
+list($major, $minor, $patch) = explode( '.', $current_version );
+switch ( $argv[1] ?? 'patch' ) {
+	case 'patch':
+		++$patch;
+		break;
+	case 'major':
+		++$major;
+		break;
+	case 'minor':
+		++$minor;
+		break;
 }
 $next_version = "$major.$minor.$patch";
 
-$files['wordpress-importer.php'] = preg_replace(
-    '/(\* Version:\s+)([\d\.]+)[ ]*/',
-    '${1}'.$next_version,
-    file_get_contents($files['wordpress-importer.php'])
+$files['wordpress-importer.php']     = preg_replace(
+	'/(\* Version:\s+)([\d\.]+)[ ]*/',
+	'${1}' . $next_version,
+	file_get_contents( $files['wordpress-importer.php'] )
 );
 $files['src/wordpress-importer.php'] = preg_replace(
-    '/(\* Version:\s+)([\d\.]+)[ ]*/',
-    '${1}'.$next_version,
-    file_get_contents($files['src/wordpress-importer.php'])
+	'/(\* Version:\s+)([\d\.]+)[ ]*/',
+	'${1}' . $next_version,
+	file_get_contents( $files['src/wordpress-importer.php'] )
 );
-$files['src/readme.txt'] = preg_replace(
-    '/(Stable tag:\s+)([\d\.]+)[ ]*/',
-    '${1}'.$next_version,
-    file_get_contents($files['src/readme.txt'])
+$files['src/readme.txt']             = preg_replace(
+	'/(Stable tag:\s+)([\d\.]+)[ ]*/',
+	'${1}' . $next_version,
+	file_get_contents( $files['src/readme.txt'] )
 );
 
-foreach ($files as $file => $new_content) {
-    file_put_contents($file, $new_content);
-    echo "Updated $file\n";
+foreach ( $files as $file => $new_content ) {
+	file_put_contents( $file, $new_content );
+	echo "Updated $file\n";
 }
 
 echo "Bumped version from $current_version to $next_version\n";
