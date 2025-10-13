d197cb092b0757a7598057ea4f9f0c920b8d43d9
Adam Zieliński <adam@adamziel.com>
2025-10-13 15:51:05 +0200

    Extract process_comment as a separate method

--- src/class-wp-import.php
+++ src/class-wp-import.php
@@ -914,53 +914,8 @@ class WP_Import extends WP_Importer {
 
 			// add/update comments
 			if ( ! empty( $post['comments'] ) ) {
-				$num_comments      = 0;
-				$inserted_comments = array();
-				foreach ( $post['comments'] as $comment ) {
-					$comment_id                                    = $comment['comment_id'];
-					$newcomments[ $comment_id ]['comment_post_ID'] = $comment_post_id;
-					$newcomments[ $comment_id ]['comment_author']  = $comment['comment_author'];
-					$newcomments[ $comment_id ]['comment_author_email'] = $comment['comment_author_email'];
-					$newcomments[ $comment_id ]['comment_author_IP']    = $comment['comment_author_IP'];
-					$newcomments[ $comment_id ]['comment_author_url']   = $comment['comment_author_url'];
-					$newcomments[ $comment_id ]['comment_date']         = $comment['comment_date'];
-					$newcomments[ $comment_id ]['comment_date_gmt']     = $comment['comment_date_gmt'];
-					$newcomments[ $comment_id ]['comment_content']      = $comment['comment_content'];
-					$newcomments[ $comment_id ]['comment_approved']     = $comment['comment_approved'];
-					$newcomments[ $comment_id ]['comment_type']         = $comment['comment_type'];
-					$newcomments[ $comment_id ]['comment_parent']       = $comment['comment_parent'];
-					$newcomments[ $comment_id ]['commentmeta']          = isset( $comment['commentmeta'] ) ? $comment['commentmeta'] : array();
-					if ( isset( $this->processed_authors[ $comment['comment_user_id'] ] ) ) {
-						$newcomments[ $comment_id ]['user_id'] = $this->processed_authors[ $comment['comment_user_id'] ];
-					}
-				}
-				ksort( $newcomments );
-
-				foreach ( $newcomments as $key => $comment ) {
-					// if this is a new post we can skip the comment_exists() check
-					if ( ! $post_exists || ! comment_exists( $comment['comment_author'], $comment['comment_date'] ) ) {
-						if ( isset( $inserted_comments[ $comment['comment_parent'] ] ) ) {
-							$comment['comment_parent'] = $inserted_comments[ $comment['comment_parent'] ];
-						}
-
-						$comment_data = wp_slash( $comment );
-						unset( $comment_data['commentmeta'] ); // Handled separately, wp_insert_comment() also expects `comment_meta`.
-						$comment_data = wp_filter_comment( $comment_data );
-
-						$inserted_comments[ $key ] = wp_insert_comment( $comment_data );
-
-						do_action( 'wp_import_insert_comment', $inserted_comments[ $key ], $comment, $comment_post_id, $post );
-
-						foreach ( $comment['commentmeta'] as $meta ) {
-							$value = $this->maybe_unserialize( $meta['value'] );
-
-							add_comment_meta( $inserted_comments[ $key ], wp_slash( $meta['key'] ), wp_slash_strings_only( $value ) );
-						}
-
-						++$num_comments;
-					}
-				}
-				unset( $newcomments, $inserted_comments, $post['comments'] );
+				$this->process_post_comments( $post['comments'], (bool) $post_exists, $comment_post_id, $post );
+				unset( $post['comments'] );
 			}
 
 			if ( ! isset( $post['postmeta'] ) ) {
@@ -1003,6 +958,122 @@ class WP_Import extends WP_Importer {
 		}
 
 		unset( $this->posts );
+	}
+
+	/**
+	 * Process comments for a post being imported.
+	 *
+	 * @param array $comments        Comment data from the WXR file.
+	 * @param bool  $post_exists     Whether the post already exists.
+	 * @param int   $comment_post_id Local post ID for the imported comments.
+	 * @param array $post            Original post array from the WXR file.
+	 */
+	protected function process_post_comments( $comments, $post_exists, $comment_post_id, $post ) {
+		$num_comments      = 0;
+		$newcomments       = array();
+		$inserted_comments = array();
+
+		foreach ( $comments as $comment ) {
+			$comment_id = $comment['comment_id'];
+
+			$newcomments[ $comment_id ] = array(
+				'comment_post_ID'      => $comment_post_id,
+				'comment_author'       => $comment['comment_author'],
+				'comment_author_email' => $comment['comment_author_email'],
+				'comment_author_IP'    => $comment['comment_author_IP'],
+				'comment_author_url'   => $comment['comment_author_url'],
+				'comment_date'         => $comment['comment_date'],
+				'comment_date_gmt'     => $comment['comment_date_gmt'],
+				'comment_content'      => $comment['comment_content'],
+				'comment_approved'     => $comment['comment_approved'],
+				'comment_type'         => $comment['comment_type'],
+				'comment_parent'       => $comment['comment_parent'],
+				'commentmeta'          => isset( $comment['commentmeta'] ) ? $comment['commentmeta'] : array(),
+			);
+
+			if ( isset( $this->processed_authors[ $comment['comment_user_id'] ] ) ) {
+				$newcomments[ $comment_id ]['user_id'] = $this->processed_authors[ $comment['comment_user_id'] ];
+			}
+		}
+
+		if ( empty( $newcomments ) ) {
+			return;
+		}
+
+		ksort( $newcomments );
+
+		foreach ( $newcomments as $key => $comment ) {
+			if ( isset( $inserted_comments[ $comment['comment_parent'] ] ) ) {
+				$comment['comment_parent'] = $inserted_comments[ $comment['comment_parent'] ];
+			}
+
+			$inserted_comment_id = $this->process_post_comment( $comment, $post_exists, $comment_post_id, $post );
+
+			if ( $inserted_comment_id ) {
+				$this->process_post_comment_metas( $inserted_comment_id, $comment['commentmeta'] );
+				$inserted_comments[ $key ] = $inserted_comment_id;
+				++$num_comments;
+			}
+		}
+	}
+
+	/**
+	 * Insert an individual comment for the post during import.
+	 *
+	 * @param array $comment         Comment data to insert.
+	 * @param bool  $post_exists     Whether the post already exists.
+	 * @param int   $comment_post_id Local post ID for the imported comment.
+	 * @param array $post            Original post array from the WXR file.
+	 * @return int|false Inserted comment ID on success, false otherwise.
+	 */
+	protected function process_post_comment( $comment, $post_exists, $comment_post_id, $post ) {
+		if ( $post_exists && comment_exists( $comment['comment_author'], $comment['comment_date'] ) ) {
+			return false;
+		}
+
+		$comment['comment_post_ID'] = $comment_post_id;
+
+		$comment_data = wp_slash( $comment );
+		unset( $comment_data['commentmeta'] ); // Handled separately, wp_insert_comment() also expects `comment_meta`.
+		$comment_data = wp_filter_comment( $comment_data );
+
+		$inserted_comment_id = wp_insert_comment( $comment_data );
+
+		do_action( 'wp_import_insert_comment', $inserted_comment_id, $comment, $comment_post_id, $post );
+
+		return $inserted_comment_id;
+	}
+
+	/**
+	 * Process comment meta for an imported comment.
+	 *
+	 * @param int   $comment_id   ID of the comment being imported.
+	 * @param array $commentmeta  Comment meta data for the inserted comment.
+	 */
+	protected function process_post_comment_metas( $comment_id, $commentmeta ) {
+		if ( empty( $commentmeta ) ) {
+			return;
+		}
+
+		foreach ( $commentmeta as $meta ) {
+			$this->process_post_comment_meta( $comment_id, $meta );
+		}
+	}
+
+	/**
+	 * Process a single comment meta entry for an imported comment.
+	 *
+	 * @param int   $comment_id ID of the comment being imported.
+	 * @param array $meta       Single meta entry (key/value) for the comment.
+	 */
+	protected function process_post_comment_meta( $comment_id, $meta ) {
+		if ( ! isset( $meta['key'], $meta['value'] ) ) {
+			return;
+		}
+
+		$value = $this->maybe_unserialize( $meta['value'] );
+
+		add_comment_meta( $comment_id, wp_slash( $meta['key'] ), wp_slash_strings_only( $value ) );
 	}
 
 	/**
